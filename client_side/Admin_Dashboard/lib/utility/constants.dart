f149233a3f33c5dd5940a47237833ae17c61e84a
Kartik-Limbachiya <117003231+Kartik-Limbachiya@users.noreply.github.com>
2025-03-30 22:10:26 +0530

    Updated constants.dart for online_store_api

--- client_side/Admin_Dashboard/lib/utility/constants.dart
+++ client_side/Admin_Dashboard/lib/utility/constants.dart
@@ -5,4 +5,4 @@ const secondaryColor = Color(0xFF2A2D3E);
 const bgColor = Color(0xFF212332);
 const defaultPadding = 16.0;
 
-const MAIN_URL = 'https://ecommerce-server-1-u7rx.onrender.com';
+const MAIN_URL = 'https://decordash.onrender.com';
