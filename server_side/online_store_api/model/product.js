8418f144fedcee34574ff4e732a4cf1125b7403f
DecorDash <decordash8@gmail.com>
2025-06-29 23:08:24 +0530

    Update product.js

--- server_side/online_store_api/model/product.js
+++ server_side/online_store_api/model/product.js
@@ -1,56 +1,112 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Name is required'],
-        trim: true
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    trim: true,
+    maxlength: 200
+  },
+  description: {
+    type: String,
+    trim: true,
+    maxlength: 2000
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0.01
+  },
+  offerPrice: {
+    type: Number,
+    min: 0.01,
+    validate: {
+      validator: function(v) {
+        return v <= this.price;
+      },
+      message: 'Offer price cannot be higher than regular price'
+    }
+  },
+  proCategoryId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+    index: true
+  },
+  proSubCategoryId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'SubCategory',
+    required: true,
+    index: true
+  },
+  proBrandId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Brand',
+    index: true
+  },
+  proVariantTypeId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'VariantType'
+  },
+  proVariantId: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Variant'
+  }],
+  images: [{
+    image: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5
     },
-    description: {
-        type: String,
-        trim: true
-    },
-    quantity: {
-        type: Number,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    offerPrice: {
-        type: Number
-    },
-    proCategoryId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category',
-        required: true
-    },
-    proSubCategoryId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'SubCategory',
-        required: true
-    },
-    proBrandId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Brand'
-    },
-    proVariantTypeId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'VariantType'
-    },
-    proVariantId: [String],
-    images: [{
-        image: {
-            type: Number,
-            required: true
-        },
-        url: {
-            type: String,
-            required: true
-        }
-    }]
-}, { timestamps: true });
+    url: {
+      type: String,
+      required: true,
+      validate: {
+        validator: v => /^https?:\/\/.+\..+/.test(v),
+        message: 'Invalid image URL format'
+      }
+    }
+  }],
+  sku: {
+    type: String,
+    unique: true,
+    sparse: true,
+    trim: true
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false
+  },
+  popularity: {
+    type: Number,
+    default: 0,
+    min: 0
+  },
+  attributes: mongoose.Schema.Types.Mixed // For dynamic properties
+}, { 
+  timestamps: true,
+  toJSON: { virtuals: true }
+});
+
+// Virtual for discount percentage
+productSchema.virtual('discountPercentage').get(function() {
+  if (this.offerPrice && this.price) {
+    return Math.round(((this.price - this.offerPrice) / this.price) * 100);
+  }
+  return 0;
+});
+
+// Indexes for search and filtering
+productSchema.index({ name: 'text', description: 'text' });
+productSchema.index({ price: 1 });
+productSchema.index({ offerPrice: 1 });
+productSchema.index({ popularity: -1 });
 
 const Product = mongoose.model('Product', productSchema);
 
