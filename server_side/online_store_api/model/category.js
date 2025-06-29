e9ec29f82b68befb2901f075cb3bb276aed21fc0
DecorDash <decordash8@gmail.com>
2025-06-29 23:03:47 +0530

    Update category.js

--- server_side/online_store_api/model/category.js
+++ server_side/online_store_api/model/category.js
@@ -1,8 +1,32 @@
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    image: { type: String, required: true }
-}, { timestamps: true });
+  name: { 
+    type: String, 
+    required: true,
+    unique: true
+  },
+  image: { type: String, required: true },
+  isFeatured: {
+    type: Boolean,
+    default: false
+  },
+  displayOrder: {
+    type: Number,
+    default: 0
+  }
+}, { 
+  timestamps: true,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
+});
+
+// Add virtual for subcategories count
+categorySchema.virtual('subcategoryCount', {
+  ref: 'SubCategory',
+  localField: '_id',
+  foreignField: 'categoryId',
+  count: true
+});
 
 module.exports = mongoose.model('Category', categorySchema);
