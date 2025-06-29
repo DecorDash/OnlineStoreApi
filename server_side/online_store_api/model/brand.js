dc4e5d7d905256db8984ee1332c3566ead1a951b
DecorDash <decordash8@gmail.com>
2025-06-29 23:03:19 +0530

    Update brand.js

--- server_side/online_store_api/model/brand.js
+++ server_side/online_store_api/model/brand.js
@@ -1,20 +1,35 @@
 const mongoose = require('mongoose');
 
-// Define the Brand schema
 const brandSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Name is required'], // Adding custom error message
-        trim: true
-    },
-    subcategoryId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'SubCategory', // This should match the model name you use when you create the SubCategory model
-        required: [true, 'Subcategory ID is required']
-    }
-},{ timestamps: true });
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    trim: true,
+    unique: true
+  },
+  subcategoryId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'SubCategory',
+    required: [true, 'Subcategory ID is required']
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  }
+}, { 
+  timestamps: true,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
+});
 
-// Create the Brand model
+// Add virtual for product count
+brandSchema.virtual('productCount', {
+  ref: 'Product',
+  localField: '_id',
+  foreignField: 'proBrandId',
+  count: true
+});
+
 const Brand = mongoose.model('Brand', brandSchema);
 
 module.exports = Brand;
